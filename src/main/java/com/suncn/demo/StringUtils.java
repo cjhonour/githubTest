21a72aabed65ed0feb403288f3d6a4b22b2f1dbf
cai <cai@DESKTOP-QMVQ9G0>
2019-10-16 02:54:19 +0800

    第一次提交

---
+++ src/main/java/com/suncn/demo/StringUtils.java
@@ -0,0 +1,10 @@
+package com.suncn.demo;
+
+import org.apache.commons.lang.StringEscapeUtils;
+
+public class StringUtils {
+	public static void main(String[] args) {
+		String string = StringEscapeUtils.escapeHtml("<");
+		System.out.println(string);
+	}
+}
